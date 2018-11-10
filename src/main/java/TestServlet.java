c30195ce4cdbe2905389c2c0e1ce11dc416e7428
Havlund <lallan.havlund@hotmail.se>
2018-11-10 05:39:59 +0100

    Simple Init

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class TestServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        response.getWriter().println("It works!");
+    }
+}
---
+++ src/main/java/TestServlet2.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/roulette")
+public class TestServlet2 extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        response.getWriter().println("It works!");
+    }
+}
